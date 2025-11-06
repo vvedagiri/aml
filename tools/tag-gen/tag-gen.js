4602f92fdf2d1f7ffe2d2befda3f04aaafded612
vvedagiri <vedagiri@adobe.com>
2025-11-06 16:03:29 +0530

    Update tag-gen.js

--- tools/tag-gen/tag-gen.js
+++ tools/tag-gen/tag-gen.js
@@ -6,9 +6,9 @@ import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 
   console.log(org, repo, path, token);
 
-  // const cmp = document.createElement('adl-tag-gen');
-  // cmp.path = `/${org}/${repo}${path}`;
-  // cmp.token = token;
+  const cmp = document.createElement('adl-tag-gen');
+  cmp.path = `/${org}/${repo}${path}`;
+  cmp.token = token;
 
-  // document.body.append(cmp);
+  document.body.append(cmp);
 }());
